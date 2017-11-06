6f167999f34243ce0f2074a2509d1f8502a67b6c
Olaf Krawczyk <radeko94@gmail.com>
2017-11-06 12:04:42 +0100

    added clientTO and reservationTO

---
+++ src/main/java/com/okrawczy/restaurantsfinder/tos/ClientTO.java
@@ -0,0 +1,44 @@
+package com.okrawczy.restaurantsfinder.tos;
+
+/**
+ * Created by Olaf on 2017-11-06.
+ */
+public class ClientTO {
+
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/okrawczy/restaurantsfinder/tos/ReservationTO.java
@@ -0,0 +1,66 @@
+package com.okrawczy.restaurantsfinder.tos;
+
+import com.okrawczy.restaurantsfinder.domain.ReservationStatus;
+
+import java.util.Date;
+
+/**
+ * Created by Olaf on 2017-11-06.
+ */
+public class ReservationTO {
+
+    private Long id;
+    private ReservationStatus status;
+    private ClientTO client;
+    private RestaurantTO restaurant;
+    private Date reservationDate;
+    private Date creationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public ClientTO getClient() {
+        return client;
+    }
+
+    public void setClient(ClientTO client) {
+        this.client = client;
+    }
+
+    public RestaurantTO getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(RestaurantTO restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
